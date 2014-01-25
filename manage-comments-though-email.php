e9ed196d083d1270d3cad179573311ccee445a0a
Ivan Lopez <ivanlopezvp@gmail.com>
2014-01-25 11:49:16 -0500

    Initial commit

---
+++ manage-comments-though-email.php
@@ -0,0 +1,33 @@
+<?php 
+
+/*
+Plugin Name: Mange Comments Through Email
+Plugin URI: http://IvanLopezDeveloper.com/
+Description: Easy mange your blog comments directly from your email. Quickly approve, reply, mark as span or trash any comment without ever having to leave your inbox.
+Version: 0.1
+Author: Ivan Lopez
+Author URI: http://IvanLopezDeveloper.com/
+*/
+
+/**
+ * Copyright (c) 2014 Ivan Lopez. All rights reserved.
+ *
+ * Released under the GPL license
+ * http://www.opensource.org/licenses/gpl-license.php
+ *
+ * This is an add-on for WordPress
+ * http://wordpress.org/
+ *
+ * **********************************************************************
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * **********************************************************************
+ */
+
